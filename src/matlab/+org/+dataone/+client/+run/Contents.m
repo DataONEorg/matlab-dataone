068e96b93dd9a14213b966216b2768c299029a6a
Christopher Jones <csj@christopherjones.co>
2014-09-15 18:03:58 -0600

    For the toolbox help documentation system, add Contents.m files for each of the packages.  The nested nature of the classes is different than many matlab toolboxes, but perhaps we'll add a top-level Contents.m file that concatenates these.

---
+++ src/matlab/+org/+dataone/+client/+run/Contents.m
@@ -0,0 +1,6 @@
+% DataONE Toolbox for MATLAB
+% Version 0.1.0 15-Sep-2014
+% Copyright (c) DataONE and participating institutions
+%
+% Classes
+%   RunManager - A class used to manage per-run outputs and products
---
+++ src/matlab/+org/+dataone/+client/+store/Contents.m
@@ -0,0 +1,6 @@
+% DataONE Toolbox for MATLAB
+% Version 0.1.0 15-Sep-2014
+% Copyright (c) DataONE and participating institutions
+%
+% Classes
+%   TripleStore - An abstract class used as an interface to define storage functions for triple statements
