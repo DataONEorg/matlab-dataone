84bad719ea70c9017d7d4848b13b70c1c45c3a42
sycao5 <sycao5@gmail.com>
2015-03-11 15:08:40 -0500

    modify set() and get() functions

---
+++ plotter.m
@@ -0,0 +1,5 @@
+% this mfile calculates and plots the 1
+% function sin(x) for 0<=x<=6. 2
+x=0:0.1:6; 
+y=sin(x); 
+plot(x,y);
--- src/matlab/+org/+dataone/+client/+configure/Session.m
+++ src/matlab/+org/+dataone/+client/+configure/Session.m
@@ -18,74 +18,76 @@
 % See the License for the specific language governing permissions and
 % limitations under the License.
 
-classdef Session %< hgsetget & dynamicprops 
+classdef Session < hgsetget %& dynamicprops 
     % SESSION A class that stores configuration settings for script runs managed through the RunManager 
     
     properties
         
-        debug = true; % A boolean property that enables or disables debugging 
-        
-        sourceRepositoryBaseURL = ''; % The base URL of the DataONE Member Node repo with source datasets for reading
-        
-        targetRepositoryBaseURL = ''; % The base URL of the DataONE Member Node repo with source datasets for writing
-        
-        x509CertificatePath = ''; % The path to the user's X509 certificate for authentication to Member Node and Coordinating Node services. 
+        debug = true; % A boolean property that enables or disables debugging  
         
         % Operating system config
         account_name = ''; % The OS account username
-        
+      
+        % Science metadata config
         scimeta_template_path = ''; % The file system path to a science metadata template file
-        
+        scimeta_title = ''; % The title of the dataset being described
+        sciemta_abstract = ''; % The abstract of the dataset being described
+               
         % DataONE config
-        
-        % Identity config
-        
-        % Provenance capture config
+        member_node_base_url = ''; % The base URL of the DataONE member node server used to store and retrieve files
+        coordinating_node_base_url = ''; % The base URL of the DataONE coordinating node server
+        format_id = ''; % The default object format identifier when creating system metadata and uploading files to a member node. 
+        submitter = ''; % The DataONE subject DN string of account uploading the file to the member node
+        rights_holder = ''; % The DataONE subject DN string of account with read/write/change permissions for the file being uploaded
+        public_read_allowed = true; % allow public read access to uploaded fiels (default: true)
+        replication_allowed = ''; % allow replicattion of files to preserve the integrity of the data file over time
+        number_of_replicas = 0; % The desired number of replicas of each file uploaded to the DataONE network
+        preferred_replica_node_list = ''; % A comma-separated list of member node identifiers that are preferred for replica storage
+        blocked_replica_node_list = ''; % A comma-separated list of member node identifiers that are blocked from replica storage
                 
+        % Identity config
+        orcid_identifier = ''; % The researcher's ORCID
+        subject_dn = ''; % The researcher's DataONE subject as a distinguished name string
+        certificate_path = ''; %The absolute file system path to the X509 certificate downloaded from https://cilogon.org
+        foaf_name = ''; % The friend of a friend 'name' vocabulary term
+               
+        % Provenance capture config
+        provenance_storage_directory = '~/.d1/provenance'; % The directory used to store per execution provenance information
+        capture_file_reads = true; % A flag indicating whether to trigger provenance capture for file read
+        capture_file_writes = true; % A flag indicating whether to trigger provenance capture for file write
+        capture_dataone_reads = true; % A flag indicating whether to trigger provenance capture for reading from DataONe MNRead.get()
+        capture_dataone_writes = true; % A flag indicating whether to trigger provenance capture for writing with DataONE MNStorage.create() or MNStorage.update()
+        capture_yesworkflow_comments = true; % A flag indicating whether to trigger provenance capture for YesWorkflow inline comments
+                     
     end
 
     methods
         
         function self = Session()
-            % SESSION A class used to set configuration options for the DataONE Toolbox
-            % Return value: self object
-            
+            % SESSION A class used to set configuration options for the DataONE Toolbox  
         end
         
-        function obj = set.x509CertificatePath(obj, path)
-            % Purpose: set the x509Certificate path for a session object
-            % Parameter: obj  the session object
-            %            path  the path value for x509 certificate
-            % Return value: a session object
-            if nargin > 0
-                obj.x509CertificatePath = strcat('test', path);
+        function obj = set(obj, name, value)
+            % SET A method used to set multiple properties at the same time
+            paraName = strtrim((name));
+            switch paraName
+                case {'member_node_base_url', 'coordinating_node_base_url'}
+                    try
+                        url = java.net.URL(paraName) % Store or save the URL contents here                           
+                    catch ME
+                        ME
+                    end
+                    
+                    obj.(name) = value;
+                    
+                otherwise
+                    obj.(name) = value;
             end
         end
         
-        function x509Path = get.x509CertificatePath(obj)
-            % Purpose: get the x509Certificate path for a session object
-            % Parameter: obj  the session object
-            % Return value: the path for x509 certificate 
-            x509Path = obj.x509CertificatePath;
-        end
-        
-        function loadSession()
-            % Purpose: Load a saved session from disk using the given filename path
-            % Parameter: path --
-            % Return value: The saved Session
-        end
-        
-        function saveSession()
-            % Purpose: Save the session to disk at the given filename path
-            % Parameter: session --
-            %            path --
-            % Return value: None
-        end
-        
-        function listSession()
-            % Purpose: Show all Session configuration parameters
-            % Parameter: session --
-            % Return value: the list of configuration parameters in the given session
+        function val = get(obj,name)
+            % GET A method used to get the value of multiple properties
+            val = obj.(name);            
         end
         
     end
