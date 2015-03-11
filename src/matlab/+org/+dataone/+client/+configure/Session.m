8611fcec3a0b6012a57a5dddf9a67848f68983db
sycao5 <sycao5@gmail.com>
2015-03-11 09:26:03 -0500

    Set ang get for x509CertificatePath property in the Session class

--- src/matlab/+org/+dataone/+client/+configure/Session.m
+++ src/matlab/+org/+dataone/+client/+configure/Session.m
@@ -18,7 +18,7 @@
 % See the License for the specific language governing permissions and
 % limitations under the License.
 
-classdef Session < hgsetget
+classdef Session %< hgsetget & dynamicprops 
     % SESSION A class that stores configuration settings for script runs managed through the RunManager 
     
     properties
@@ -29,13 +29,63 @@ classdef Session < hgsetget
         
         targetRepositoryBaseURL = ''; % The base URL of the DataONE Member Node repo with source datasets for writing
         
-        x509CertificatePath = ''; % The path to the user's X509 certificate for authentication to Member Node and Coordinating Node services.        
+        x509CertificatePath = ''; % The path to the user's X509 certificate for authentication to Member Node and Coordinating Node services. 
+        
+        % Operating system config
+        account_name = ''; % The OS account username
+        
+        scimeta_template_path = ''; % The file system path to a science metadata template file
+        
+        % DataONE config
+        
+        % Identity config
+        
+        % Provenance capture config
+                
     end
 
     methods
         
         function self = Session()
             % SESSION A class used to set configuration options for the DataONE Toolbox
+            % Return value: self object
+            
+        end
+        
+        function obj = set.x509CertificatePath(obj, path)
+            % Purpose: set the x509Certificate path for a session object
+            % Parameter: obj  the session object
+            %            path  the path value for x509 certificate
+            % Return value: a session object
+            if nargin > 0
+                obj.x509CertificatePath = strcat('test', path);
+            end
+        end
+        
+        function x509Path = get.x509CertificatePath(obj)
+            % Purpose: get the x509Certificate path for a session object
+            % Parameter: obj  the session object
+            % Return value: the path for x509 certificate 
+            x509Path = obj.x509CertificatePath;
+        end
+        
+        function loadSession()
+            % Purpose: Load a saved session from disk using the given filename path
+            % Parameter: path --
+            % Return value: The saved Session
+        end
+        
+        function saveSession()
+            % Purpose: Save the session to disk at the given filename path
+            % Parameter: session --
+            %            path --
+            % Return value: None
+        end
+        
+        function listSession()
+            % Purpose: Show all Session configuration parameters
+            % Parameter: session --
+            % Return value: the list of configuration parameters in the given session
         end
         
     end
