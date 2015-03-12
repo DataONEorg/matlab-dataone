b0b05cce628d36b69085c332347b8d127cb8d655
Christopher Jones <csj@christopherjones.co>
2015-03-11 19:55:52 -0600

    Fix a typo in the superclass name.

--- src/matlab/+org/+dataone/+client/+v1/D1Client.m
+++ src/matlab/+org/+dataone/+client/+v1/D1Client.m
@@ -21,7 +21,7 @@
 % See the License for the specific language governing permissions and
 % limitations under the License.
 
-classdef D1Client < hggetset
+classdef D1Client < hgsetget
     
     properties
     end
