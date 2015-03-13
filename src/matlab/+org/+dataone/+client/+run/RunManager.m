c95eb44b8e0208a0ff9de80d3c2718fab83eca22
Christopher Jones <csj@christopherjones.co>
2015-03-12 21:58:40 -0600

    Stub out the Run Manager API methods in the RunManager class. refs https://github.com/DataONEorg/sem-prov-design/issues/104

--- src/matlab/+org/+dataone/+client/+run/RunManager.m
+++ src/matlab/+org/+dataone/+client/+run/RunManager.m
@@ -1,4 +1,4 @@
-% RUNMANAGER A class used to manage per-run outputs and products
+% RUNMANAGER A class used to track information about program runs.
 %   The RunManager class provides functions to manage script runs in terms
 %   of the known file inputs and the derived file outputs. It keeps track
 %   of the provenance (history) relationships between these inputs and outputs.
@@ -24,22 +24,64 @@
 classdef RunManager < hgsetget
     
     properties
-        
-        configuration; % The instance of the Configuration class used to provide settings details for this RunManager
-        
+       
+        % The instance of the Session class used to provide settings 
+        % details for this RunManager
+        session;
     end
 
     methods
         
-        function self = RunManager(configuration)
+        function runManager = RunManager(session)
             % RUNMANAGER Constructor: creates an instance of the RunManager class
             %   The RunManager class manages outputs of a script based on the
             %   settings in the given configuuration instance passed in.
         
-            self.configuration = configuration
+            runManager.session = session
         end
         
-        function record(self)
+        function data_package = record(runManager)
+            % RECORD Records provenance relationships between data and scripts
+            % When record() is called, data input files, dataoutput files,
+            % and programs (scripts and classes) are tracked during an
+            % execution of the program, and a graph of their relationships
+            % is produced using the W3C PROV ontology standard 
+            % (<http://www.w3.org/TR/prov-o/>) and the
+            % DataONE ProvONE model(<https://purl.dataone.org/provone-v1-dev>).
+            
+        end
+        
+        function startRecord(runManager, tag)
+            % STARTRECORD Starts recording provenance relationships (see
+            % record()).
+
+        end
+        
+        function data_package = endRecord(runManager)
+            % ENDRECORD Ends the recording of an execution (run).
+        end
+        
+        function runs = listRuns(runManager, quiet, startDate, endDate, tags)
+            % LISTRUNS Lists prior executions (runs) and information about them.
+        end
+        
+        function deleted_list = deleteRuns(runIdList, startDate, endDate, tags)
+            % DELETERUNS Deletes prior executions (runs) from the stored
+            % list.
+            
+        end
+        
+        function package_id = view(runManager, packageId)
+            % VIEW Displays detailed information about a data package that
+            % is the result of an execution (run).
+            
+        end
+        
+        function package_id = publish(runManager, packageId)
+            % PUBLISH Uploads a data package produced by an execution (run)
+            % to the configured DataONE Member Node server.
+            
+        end
     end
     
 end
