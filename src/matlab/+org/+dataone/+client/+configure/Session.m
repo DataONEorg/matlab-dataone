5cdc3d98d603b47288add6acd0a3165c4e4df31e
Christopher Jones <csj@christopherjones.co>
2015-03-22 21:59:56 -0600

    Reformat the Session class' documentation to follow the Matlab documentation conventions.

--- src/matlab/+org/+dataone/+client/+configure/Session.m
+++ src/matlab/+org/+dataone/+client/+configure/Session.m
@@ -20,47 +20,101 @@
 
 classdef Session < hgsetget %& dynamicprops 
     % SESSION A class that stores configuration settings for script runs managed through the RunManager 
+    
     properties
         
         debug = true; % A boolean property that enables or disables debugging  
         
-        % Operating system config
-        account_name = ''; % The OS account username
+        %% Operating system configuration
+        
+        % The Operating System account username
+        account_name = '';
       
-        % Science metadata config
-        scimeta_template_path = ''; % The file system path to a science metadata template file
-        scimeta_title = ''; % The title of the dataset being described
-        sciemta_abstract = ''; % The abstract of the dataset being described
-               
-        % DataONE config        
-        source_member_node_id  = 'urn:node:'; % The source member node identifier
-        target_member_node_id = 'urn:node:'; % The target member node identifier
-        format_id = 'application/octet-stream'; % The default object format identifier when creating system metadata and uploading files to a member node. 
-        submitter = ''; % The DataONE subject DN string of account uploading the file to the member node
-        rights_holder = ''; % The DataONE subject DN string of account with read/write/change permissions for the file being uploaded
-        public_read_allowed = true; % allow public read access to uploaded fiels (default: true)
-        replication_allowed = true; % allow replicattion of files to preserve the integrity of the data file over time
-        number_of_replicas = 2; % The desired number of replicas of each file uploaded to the DataONE network
-        preferred_replica_node_list = ''; % A comma-separated list of member node identifiers that are preferred for replica storage
-        blocked_replica_node_list = ''; % A comma-separated list of member node identifiers that are blocked from replica storage
-        coordinating_node_base_url = 'https://cn-sandbox-2.test.dataone.org/cn'; % The base URL of the DataONE coordinating node server
+        %% Science metadata configuration
         
-        % Identity config
-        orcid_identifier = ''; % The researcher's ORCID
-        subject_dn = ''; % The researcher's DataONE subject as a distinguished name string
-        certificate_path = ''; %The absolute file system path to the X509 certificate downloaded from https://cilogon.org
-        foaf_name = ''; % The friend of a friend 'name' vocabulary term
-               
-        % Provenance capture config
-        provenance_storage_directory = '~/.d1/provenance'; % The directory used to store per execution provenance information
-        capture_file_reads = true; % A flag indicating whether to trigger provenance capture for file read
-        capture_file_writes = true; % A flag indicating whether to trigger provenance capture for file write
-        capture_dataone_reads = true; % A flag indicating whether to trigger provenance capture for reading from DataONe MNRead.get()
-        capture_dataone_writes = true; % A flag indicating whether to trigger provenance capture for writing with DataONE MNStorage.create() or MNStorage.update()
-        capture_yesworkflow_comments = true; % A flag indicating whether to trigger provenance capture for YesWorkflow inline comments
+        % The file system path to a science metadata template file
+        scimeta_template_path = '';
         
-        % Session storage config
-        persistent_session_file_name = ''; % The directory used to store persistent session file Eg: $HOME/.d1/session.json
+        % The title of the dataset being described
+        scimeta_title = '';
+        
+        % The abstract of the dataset being described
+        sciemta_abstract = ''; 
+               
+        %% DataONE configuration        
+        
+        % The source member node identifier
+        source_member_node_id  = 'urn:node:';
+        
+        % The target member node identifier
+        target_member_node_id = 'urn:node:';
+        
+        % The default object format identifier when creating system metadata and uploading files to a member node. 
+        format_id = 'application/octet-stream';
+        
+        % The DataONE subject DN string of account uploading the file to the member node
+        submitter = '';
+        
+        % The DataONE subject DN string of account with read/write/change permissions for the file being uploaded
+        rights_holder = '';
+        
+        % Allow public read access to uploaded fiels (default: true)
+        public_read_allowed = true;
+        
+        % Allow replicattion of files to preserve the integrity of the data file over time
+        replication_allowed = true;
+        
+        % The desired number of replicas of each file uploaded to the DataONE network
+        number_of_replicas = 2;
+        
+        % A comma-separated list of member node identifiers that are preferred for replica storage
+        preferred_replica_node_list = '';
+        
+        % A comma-separated list of member node identifiers that are blocked from replica storage
+        blocked_replica_node_list = '';
+        
+        % The base URL of the DataONE coordinating node server
+        coordinating_node_base_url = 'https://cn-sandbox-2.test.dataone.org/cn';
+        
+        %% Identity configuration
+        
+        % The researcher's ORCID
+        orcid_identifier = '';
+        
+        % The researcher's DataONE subject as a distinguished name string
+        subject_dn = '';
+        
+        %The absolute file system path to the X509 certificate downloaded from https://cilogon.org
+        certificate_path = '';
+        
+        % The friend of a friend 'name' vocabulary term
+        foaf_name = '';
+               
+        %% Provenance capture configuration
+        
+        % The directory used to store per execution provenance information
+        provenance_storage_directory = '~/.d1/provenance';
+        
+        % A flag indicating whether to trigger provenance capture for file read
+        capture_file_reads = true;
+        
+        % A flag indicating whether to trigger provenance capture for file write
+        capture_file_writes = true;
+        
+        % A flag indicating whether to trigger provenance capture for reading from DataONe MNRead.get()
+        capture_dataone_reads = true;
+        
+        % A flag indicating whether to trigger provenance capture for writing with DataONE MNStorage.create() or MNStorage.update()
+        capture_dataone_writes = true;
+        
+         % A flag indicating whether to trigger provenance capture for YesWorkflow inline comments
+        capture_yesworkflow_comments = true;
+        
+        %% Session storage configuration
+        
+        % The directory used to store persistent session file Eg: $HOME/.d1/session.json
+        persistent_session_file_name = '';
+        
     end
 
     methods(Static)
@@ -178,9 +232,7 @@ classdef Session < hgsetget %& dynamicprops
         
         %-------------------------------------------------------------------------------------------
         function session = saveSession(session)
-            % SAVESESSION 
-            
-            fprintf('\n[In saveSession().]');
+            % SAVESESSION Saves the session properties to a JSON file
             
             % Convert session object to session struct
             sessionProps = properties(session); % displays the names of the public properties for the class of session
