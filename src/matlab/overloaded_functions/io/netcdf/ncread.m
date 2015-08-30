6fbb3022c2e6b242476d8b85a00c6ca5ceb2ec8c
Christopher Jones <csj@christopherjones.co>
2015-08-30 08:36:45 -0600

    Add a directory to store overloaded functions, and begin with the ncread function that is a wrapper to the ncread function shipped with Matlab.  During record(), this wrapper function is called in preference to the shipped version because of its order on the Matlab path.  It in turn calls the shipped function.

---
+++ src/matlab/overloaded_functions/io/netcdf/ncread.m
@@ -0,0 +1,45 @@
+function vardata = ncread( source, varname, varargin )
+% NCREAD A provenance wrapper function to the builtin NetCDF ncread
+%    VARDATA = NCREAD(FILENAME,VARNAME) reads data from the variable
+%    VARNAME in the NetCDF file FILENAME, and generates provenance
+%    information about the read event.
+%
+%    VARDATA = NCREAD(OPENDAP_URL,VARNAME) reads data from the variable
+%    VARNAME from an OPeNDAP NetCDF data source, and generates provenance
+%    information about the read event.
+%
+%    VARDATA = NCREAD(SOURCE,VARNAME,START, COUNT) 
+%    VARDATA = NCREAD(SOURCE,VARNAME,START, COUNT, STRIDE) reads data from
+%    VARNAME beginning at the location given by START from SOURCE, which
+%    can either be a filename or an OPeNDAP URL, and generates provenance
+%    information about the read event. For an N-dimensional
+%    variable START is a vector of 1-based indices of length N specifying
+%    the starting location. COUNT is also a vector of length N specifying
+%    the number of elements to read along corresponding dimensions. If a
+%    particular element of COUNT is Inf, data is read until the end of that
+%    dimension. The optional argument STRIDE specifies the inter-element
+%    spacing along each dimension. STRIDE defaults to a vector of ones.
+%
+% This work was created by participants in the DataONE project, and is
+% jointly copyrighted by participating institutions in DataONE. For
+% more information on DataONE, see our web site at http://dataone.org.
+%
+%   Copyright 2015 DataONE
+%
+% Licensed under the Apache License, Version 2.0 (the "License");
+% you may not use this file except in compliance with the License.
+% You may obtain a copy of the License at
+%
+%   http://www.apache.org/licenses/LICENSE-2.0
+%
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS,
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+% See the License for the specific language governing permissions and
+% limitations under the License.
+
+    disp('Called the ncread wrapper function.');
+
+    vardata = builtin('ncread', source, varname, varargin);
+end
+
