a9cf0d8966d9e75a6c49f254a454d4606988ff12
Christopher Jones <csj@christopherjones.co>
2015-08-30 21:08:47 -0600

    Back out the ProvenanceTracker classes for now in preference of using the RunManager directly to track provenance.

--- src/matlab/+org/+dataone/+client/+track/Contents.m
+++
@@ -1,7 +0,0 @@
-% DataONE Toolbox for MATLAB
-% Version 0.1.0 15-Sep-2014
-% Copyright (c) DataONE and participating institutions
-%
-% Functions
-%   PROVENANCETRACKER defines the interface for tracking provenance
-%   FILETRACKER tracks file-level input/output information
