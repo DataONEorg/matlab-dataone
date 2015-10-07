59e6361fc255fdb11066b3ae56caade72f44b220
Christopher Jones <csj@christopherjones.co>
2015-10-07 15:58:10 -0600

    Add the Toolbox Contents file for the org.ecoinformatics.eml package.

---
+++ src/matlab/+org/+ecoinformatics/+eml/Contents.m
@@ -0,0 +1,6 @@
+% DataONE Toolbox for MATLAB
+% Version 0.1.0 15-Sep-2014
+% Copyright (c) DataONE and participating institutions
+%
+% Classes
+%   EML - A class used to model an Ecological Metadata Language document.
