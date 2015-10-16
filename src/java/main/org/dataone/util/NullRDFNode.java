ae52509af7dd43956a5fd53ce40c68a690c1d0e8
Christopher Jones <csj@christopherjones.co>
2015-10-16 14:54:43 -0600

    Add compiled versions of the wrapper classes to the lib/classes directory so they get on the Java class path.  Remove un-needed imports in NullRDFNode.
    refs https://github.com/DataONEorg/sem-prov-design/issues/175

--- src/java/main/org/dataone/util/NullRDFNode.java
+++ src/java/main/org/dataone/util/NullRDFNode.java
@@ -1,21 +1,6 @@
 package org.dataone.util;
 
-import java.net.URISyntaxException;
-import java.util.HashSet;
-import java.util.Set;
-
-import org.dspace.foresite.Predicate;
-
-import com.hp.hpl.jena.graph.Node;
-import com.hp.hpl.jena.graph.Triple;
-import com.hp.hpl.jena.rdf.model.Model;
-import com.hp.hpl.jena.rdf.model.ModelFactory;
-import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.RDFNode;
-import com.hp.hpl.jena.rdf.model.Statement;
-import com.hp.hpl.jena.rdf.model.StmtIterator;
-import com.hp.hpl.jena.sparql.util.Base64.InputStream;
-import com.hp.hpl.jena.util.FileManager;
 
 /**
  * A temporary class that provides an RDFNode static member that is set to null because
