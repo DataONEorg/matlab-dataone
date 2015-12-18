fc62ea043d6524687b0e0280f64e6bd00d277069
Christopher Jones <csj@christopherjones.co>
2015-12-18 08:58:31 -0700

    Make the internal cnode property private - outside access shouldn't be needed.
    refs https://github.com/DataONEorg/sem-prov-design/issues/186

--- src/matlab/+org/+dataone/+client/+v2/CoordinatingNode.m
+++ src/matlab/+org/+dataone/+client/+v2/CoordinatingNode.m
@@ -2,10 +2,14 @@ classdef CoordinatingNode < org.dataone.client.v2.DataONENode
     %COORDINATINGNODE A class representing a DataONE Coordinating Node
     
     properties
+                
+    end
+    
+    properties (Access = 'private')
         
         % The Java CNode instance
         cnode;
-        
+
     end
     
     methods
