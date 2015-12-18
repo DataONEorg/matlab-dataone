9c7e07f4bc45f22d8e5337bad7e7ccd45e31cd09
Christopher Jones <csj@christopherjones.co>
2015-12-18 16:02:24 -0700

    Remove the cnode property in favor of the superclass node property.
    refs https://github.com/DataONEorg/sem-prov-design/issues/186

--- src/matlab/+org/+dataone/+client/+v2/CoordinatingNode.m
+++ src/matlab/+org/+dataone/+client/+v2/CoordinatingNode.m
@@ -2,14 +2,7 @@ classdef CoordinatingNode < org.dataone.client.v2.DataONENode
     %COORDINATINGNODE A class representing a DataONE Coordinating Node
     
     properties
-                
-    end
-    
-    properties (Access = 'private')
         
-        % The Java CNode instance
-        cnode;
-
     end
     
     methods
