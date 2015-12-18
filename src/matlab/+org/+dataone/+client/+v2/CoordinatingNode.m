f7c818db7649f27a8555071f40273b5c277786f3
Christopher Jones <csj@christopherjones.co>
2015-12-18 08:22:20 -0700

    Stub out the CoordinatingNode class.
    refs https://github.com/DataONEorg/sem-prov-design/issues/186

---
+++ src/matlab/+org/+dataone/+client/+v2/CoordinatingNode.m
@@ -0,0 +1,15 @@
+classdef CoordinatingNode < org.dataone.client.v2.DataONENode
+    %COORDINATINGNODE A class representing a DataONE Coordinating Node
+    
+    properties
+        
+        % The Java CNode instance
+        cnode;
+        
+    end
+    
+    methods
+    end
+    
+end
+
