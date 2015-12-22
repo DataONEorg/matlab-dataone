f165aa5a9cd289d89c036f432f24c5590dccb5fd
Christopher Jones <csj@christopherjones.co>
2015-12-22 11:48:39 -0700

    Fix the superclass reference to use the fully qualified class name for the DataONENode superclass.
    refs https://github.com/DataONEorg/sem-prov-design/issues/186

--- src/matlab/+org/+dataone/+client/+v2/MemberNode.m
+++ src/matlab/+org/+dataone/+client/+v2/MemberNode.m
@@ -22,7 +22,7 @@
 % See the License for the specific language governing permissions and
 % limitations under the License.
 
-classdef MemberNode < DataONENode
+classdef MemberNode < org.dataone.client.v2.DataONENode
     
     properties
                 
