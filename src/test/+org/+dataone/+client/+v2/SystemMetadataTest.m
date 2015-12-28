a28d9d7929f0b1928e3794eef4634b1aeb50875d
Christopher Jones <csj@christopherjones.co>
2015-12-28 14:34:55 -0700

    Add in a testSet() test function to test setting all of the SystemMetadata properties.  Needs assertions next.

--- src/test/+org/+dataone/+client/+v2/SystemMetadataTest.m
+++ src/test/+org/+dataone/+client/+v2/SystemMetadataTest.m
@@ -32,9 +32,52 @@ classdef SystemMetadataTest < matlab.unittest.TestCase
             sysmeta = SystemMetadata();
             
             testCase.assertInstanceOf(sysmeta, 'org.dataone.client.v2.SystemMetadata');
-            testCase.assertTrue(isstruct(sysmeta.accessPolicy));
-            testCase.assertTrue(isstruct(sysmeta.replicationPolicy));
-            testCase.assertTrue(isstruct(sysmeta.checksum));
+            
+        end
+        
+        function testSet(testCase)
+            
+            import org.dataone.client.v2.SystemMetadata;
+            sysmeta = SystemMetadata();
+            
+            set(sysmeta, 'serialVersion', 0);
+            set(sysmeta, 'identifier', 'my-very-unique-id.1.1');
+            set(sysmeta, 'formatId', 'text/plain');
+            set(sysmeta, 'size', 1234567890);
+            checksum(1).value = 'ba74468f570cc85df03e87b098c07d048e2f19c1';
+            checksum(1).algorithm = 'SHA-1';
+            set(sysmeta, 'checksum', checksum);
+            set(sysmeta, 'submitter', 'CN=Christopher Jones A2108, O=Google, C=US, DC=cilogon, DC=org');
+            set(sysmeta, 'rightsHolder', 'CN=Christopher Jones A2108, O=Google, C=US, DC=cilogon, DC=org');
+            
+            accessPolicy.rules = containers.Map('KeyType', 'char', 'ValueType', 'char');
+            accessPolicy.rules('public') = 'read';
+            set(sysmeta, 'accessPolicy', accessPolicy);
+            
+            replicationPolicy.replicationAllowed = true;
+            replicationPolicy.numberOfReplicas = 2;
+            replicationPolicy.preferredNodes = {'urn:node:FASTNODE', 'urn:node:FRIENDNODE'};
+            replicationPolicy.blockedNodes = {'urn:node:SLOWNODE', 'urn:node:FOENODE'};
+            set(sysmeta, 'replicationPolicy', replicationPolicy);
+            
+            % set(sysmeta, 'obsoletes', '');
+            % set(sysmeta, 'obsoletedBy', '');
+            set(sysmeta, 'archived', false);
+            new_date = datetime('now', 'TimeZone', 'UTC', 'Format', 'yyyy-mm-dd''T''HH:mm:ss.SSSZ');
+            set(sysmeta, 'dateUploaded', new_date);
+            set(sysmeta, 'dateSysMetadataModified', new_date);
+            set(sysmeta, 'originMemberNode', 'urn:node:MYNODE');
+            set(sysmeta, 'authoritativeMemberNode', 'urn:node:MYNODE');
+            % sysmeta.replica = ;
+            set(sysmeta, 'seriesId', 'doi:10.5063/F12W3B8Z');
+            mediaType.name = 'text/plain';
+            mediaType.properties = containers.Map('KeyType', 'char', 'ValueType', 'char');
+            mediaType.properties('my-prop-1') = 'my-val-1';
+            mediaType.properties('my-prop-2') = 'my-val-2';
+            set(sysmeta, 'mediaType', mediaType);
+            set(sysmeta, 'fileName', 'my-file-name.txt');
+
+            xml = sysmeta.toXML()
         end
     end
     
