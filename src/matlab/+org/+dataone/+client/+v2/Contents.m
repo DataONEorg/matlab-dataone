a50fd9c37e244537305f26a4fd670f3b63a9920d
Christopher Jones <csj@christopherjones.co>
2016-01-01 19:22:16 -0700

    Update the Contents with the new Session and Coordinating Node classes.
    refs https://github.com/DataONEorg/sem-prov-design/issues/188

--- src/matlab/+org/+dataone/+client/+v2/Contents.m
+++ src/matlab/+org/+dataone/+client/+v2/Contents.m
@@ -5,4 +5,6 @@
 % Classes
 %   DataONEClient - A class used to communicate with nodes in the DataONE network
 %   DataObject - A class that represents a data object with DataONE properties
-%   MemberNode - A class that represent a member node in the DataONE network
+%   MemberNode - A class that represents a member node in the DataONE network
+%   CoordinatingNode - A class that represents a coordinating node in the DataONE network
+%   Session - A class that provides an authenticated session based on configuration settings
