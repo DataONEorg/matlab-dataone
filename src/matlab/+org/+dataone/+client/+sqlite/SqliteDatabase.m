c5b385d2601dd8ce21b7f2ff3e63600e624dec71
sycao5 <sycao5@gmail.com>
2016-08-08 18:32:43 -0500

    Change the execute( ) function in the SqliteDatabase class. For the insert operation, the curs.ResultSet=0. For the select query, the curs.ResultSet is  [1x1 org.sqlite.jdbc4.JDBC4ResultSet]

--- src/matlab/+org/+dataone/+client/+sqlite/SqliteDatabase.m
+++ src/matlab/+org/+dataone/+client/+sqlite/SqliteDatabase.m
@@ -19,17 +19,10 @@ classdef SqliteDatabase < org.dataone.client.sqlite.Database
         end
         
         function db_conn = openDBConnection(sqldb_obj, configOptions)
-            
-%             JDBC_SQLITE_DRIVER = 'org.sqlite.JDBC';
-%             DB_URL = 'jdbc:sqlite:/Users/syc/Documents/matlab-dataone/prov.db';
-%             USER = '';
-%             PASS = '';
-%             error_message = '';
-%             
+          
             try
                 sqldb_obj.dbConn = database(sqldb_obj.dbName, sqldb_obj.userName, sqldb_obj.password, sqldb_obj.JDBC_SQLITE_DRIVER, sqldb_obj.DB_URL);
-                db_conn = sqldb_obj.dbConn;
-                
+                db_conn = sqldb_obj.dbConn;                
             catch runtimeError
                 error_message = ...
                     [error_message ' ' ...
@@ -37,11 +30,9 @@ classdef SqliteDatabase < org.dataone.client.sqlite.Database
                 error(['There was an error trying to connect to the sqlite database: ' ...
                     char(10) ...
                     error_message]);
-            end
-            
+            end            
         end
-        
-        
+                
         function count = getTable(sqldb_obj, table_name)
             
             sqldb_obj.openDBConnection();
@@ -73,11 +64,17 @@ classdef SqliteDatabase < org.dataone.client.sqlite.Database
                 sqldb_obj.openDBConnection();
                 
                 curs = exec(sqldb_obj.dbConn, sql_statement);
-                curs = fetch(curs);
                 
-                if rows(curs) == 0
-                    result = [];
+                if curs.ResultSet ~= 0 % for select query (changed on 080816)
+                    curs = fetch(curs);
+                    
+                    if rows(curs) == 0
+                        result = [];
+                    else
+                        result = curs.Data;
+                    end
                 else
+                    % for read query (changed on 080816)
                     result = curs.Data;
                 end
                 
